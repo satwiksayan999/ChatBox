7b37cffc5df659ddad498ea2ba5b52dea03eed68
satwiksayan999 <68969613+satwiksayan999@users.noreply.github.com>
2024-06-30 19:16:48 +0530

    Update Signup

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-center text-gray-300'>
                     SignUp
-                    <span className='text-blue-500'> ChatApp</span>
+                    <span className='text-blue-500'> ChatBox</span>
                 </h1>
 
                 <form onSubmit={handleSubmit}>
